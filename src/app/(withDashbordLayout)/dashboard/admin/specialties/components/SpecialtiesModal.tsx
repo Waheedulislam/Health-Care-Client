89747096c2c9eea36f00e3f36f9dcd2a3a84422f
Whaeedil <baoppyhossen1234@gmail.com>
2024-12-02 19:26:10 +0600

    redux

--- src/app/(withDashbordLayout)/dashboard/admin/specialties/components/SpecialtiesModal.tsx
+++ src/app/(withDashbordLayout)/dashboard/admin/specialties/components/SpecialtiesModal.tsx
@@ -2,10 +2,12 @@ import PHFileUploader from '@/Components/Forms/PHFileUploder';
 import PHForm from '@/Components/Forms/PHForm';
 import PHInput from '@/Components/Forms/PHInput';
 import PHModal from '@/Components/Shared/PHModal/PHModal';
+import { useCreateSpecialtiesMutation } from '@/redux/api/specialtiesApi';
 import { modifyPayload } from '@/utils/modifyPayload';
 import { Button, Grid, TextField } from '@mui/material';
 import React from 'react';
 import { FieldValues } from 'react-hook-form';
+import { toast } from 'sonner';
 
 type TProps = {
     open: boolean;
@@ -14,11 +16,20 @@ type TProps = {
 
 const SpecialtyModal = ({ open, setOpen }: TProps) => {
 
-    const handleFormSubmit = (values: FieldValues) => {
+    const [createSpecialties] = useCreateSpecialtiesMutation();
+
+
+
+    const handleFormSubmit = async (values: FieldValues) => {
 
         const data = modifyPayload(values);
         try {
-
+            const res = await createSpecialties(data).unwrap();
+            if (res?.id) {
+                toast.success('Specialty created successfully !!')
+                setOpen(false);
+            }
+            console.log(res)
         } catch (err: any) {
 
         }
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,5 @@ import { createApi, } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
     reducerPath: 'api',
     baseQuery: axiosBaseQuery({ baseUrl: 'http://localhost:5000/api/v1' }),
-    endpoints: () => ({
-
-    }),
+    endpoints: () => ({}),
 })
